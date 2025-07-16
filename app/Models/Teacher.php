c86b3b46dcda1cde3931ebc5a39b9196a3c307dc
Jowen <diezjowenmiguel@gmail.com>
2025-07-16 21:04:22 +0800

    teacher table

--- app/Models/Teacher.php
+++ app/Models/Teacher.php
@@ -10,12 +10,17 @@ class Teacher extends Model
      protected $fillable = [
         'user_id',
         'name',
-        'is_advisor',
+        'is_advisor_id',
         'subject',
     ];
 
     public function user(): BelongsTo
     {
-        return $this->belongsTo(User::class);
+        return $this->belongsTo(User::class,'user_id');
     }
+    public function section(): BelongsTo
+    {
+        return $this->belongsTo(Section::class,'is_advisor_id');
+    }
+
 }
--- database/migrations/2025_07_16_120203_create_teachers_table.php
+++ database/migrations/2025_07_16_120203_create_teachers_table.php
@@ -13,9 +13,9 @@ return new class extends Migration
     {
         Schema::create('teachers', function (Blueprint $table) {
         $table->id();
-        $table->foreignId('user_id')->constrained()->onDelete('cascade');
+        $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
         $table->string('name');
-        $table->string('is_advisory')->nullable();
+        $table->foreignId('is_advisory_id')->constrained('section')->onDelete('cascade');
         $table->string('subject')->nullable();
         $table->timestamps();
         });
