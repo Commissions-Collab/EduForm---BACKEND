91f4f002d8effaa765edf9b099b5304fe6ae2198
Izuuii <denverdeleon22@gmail.com>
2025-11-18 21:26:22 +0800

    deployment

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -17,17 +17,5 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        // Add CORS headers to all exception responses
-        $exceptions->respond(function ($response, $exception, $request) {
-            // Get the frontend URL from config
-            $frontendUrl = config('cors.allowed_origins')[0] ?? 'https://acad-flow.com';
-            
-            // Add CORS headers to the response
-            $response->headers->set('Access-Control-Allow-Origin', $frontendUrl);
-            $response->headers->set('Access-Control-Allow-Credentials', 'true');
-            $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
-            $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Accept, Authorization, X-Requested-With, X-XSRF-TOKEN');
-            
-            return $response;
-        });
+        //
     })->create();
